1795aa453a3d5c4b60743e55130e7082c66a25fe
tanishaKashyap <tanisha.pankajj@gmail.com>
2025-12-12 18:10:53 +0530

    session-2 done

---
+++ G1_23BCS12542_SESSION-2/HOMEWORK/Nth_Highest_Salary.sql
@@ -0,0 +1,11 @@
+-- G1_23BCS12542_Tanisha_Kumari_SESSION-1
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+set N=N-1;
+  RETURN (
+    select  distinct  salary as getNthHighestSalary
+    from  Employee 
+    order by salary DESC
+    LIMIT 1 OFFSET N
+  );
+END
